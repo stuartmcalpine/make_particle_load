782d24e78cfe0df3e70f82a16daaafe5930c8e7d
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-10-03 01:04:31 +0100

    Minor file reformat

--- modules/ParallelFunctions.py
+++ modules/ParallelFunctions.py
@@ -1,25 +1,27 @@
 import numpy as np
 
+
 def my_alltoallv(sendbuf, send_count, send_offset, recvbuf, recv_count, recv_offset, comm,
-        comm_rank, comm_size):
+                 comm_rank, comm_size):
     """Alltooallv implemented using sendrecv calls"""
 
     # Get communicator to use
     ptask = 0
-    while(2**ptask < comm_size):
+    while 2 ** ptask < comm_size:
         ptask += 1
 
     # Loop over pairs of processes and send data
-    for ngrp in range(2**ptask):
+    for ngrp in range(2 ** ptask):
         rank = comm_rank ^ ngrp
         if rank < comm_size:
             soff = send_offset[rank]
-            sc   = send_count[rank]
+            sc = send_count[rank]
             roff = recv_offset[rank]
-            rc   = recv_count[rank]
+            rc = recv_count[rank]
             if sc > 0 or rc > 0:
-                comm.Sendrecv(sendbuf[soff:soff+sc], rank, 0,
-                              recvbuf[roff:roff+rc], rank, 0)
+                comm.Sendrecv(sendbuf[soff:soff + sc], rank, 0,
+                              recvbuf[roff:roff + rc], rank, 0)
+
 
 def repartition(arr, ndesired, comm, comm_rank, comm_size):
     """ Return the input arr repartitioned between processors. """
@@ -29,13 +31,13 @@ def repartition(arr, ndesired, comm, comm_rank, comm_size):
 
     # Sanity checks on input arrays
     if len(arr.shape) != 1:
-        print("Can only repartition 1D array data! (shape=%i)"%len(arr.shape))
+        print("Can only repartition 1D array data! (shape=%i)" % len(arr.shape))
         comm.Abort()
 
     # Find total number of elements
-    n        = arr.shape[0]
+    n = arr.shape[0]
     nperproc = comm.allgather(n)
-    ntot     = sum(nperproc)
+    ntot = sum(nperproc)
 
     # Check total doesn't change
     if ntot != sum(ndesired):
@@ -43,7 +45,7 @@ def repartition(arr, ndesired, comm, comm_rank, comm_size):
         comm.Abort()
 
     # Find first index on each processor
-    first_on_proc_in  = np.cumsum(nperproc) - nperproc
+    first_on_proc_in = np.cumsum(nperproc) - nperproc
     first_on_proc_out = np.cumsum(ndesired) - ndesired
 
     # Count elements to go to each other processor
@@ -51,12 +53,12 @@ def repartition(arr, ndesired, comm, comm_rank, comm_size):
     for rank in range(comm_size):
         # Find range of elements to go to this other processor
         ifirst = first_on_proc_out[rank]
-        ilast  = ifirst + ndesired[rank] - 1
+        ilast = ifirst + ndesired[rank] - 1
         # We can only send the elements which are stored locally
         ifirst = max((ifirst, first_on_proc_in[comm_rank]))
-        ilast  = min((ilast,  first_on_proc_in[comm_rank] +
-                      nperproc[comm_rank] - 1))
-        send_count[rank] = np.max((0,ilast-ifirst+1))
+        ilast = min((ilast, first_on_proc_in[comm_rank] +
+                     nperproc[comm_rank] - 1))
+        send_count[rank] = np.max((0, ilast - ifirst + 1))
 
     # Transfer the data
     send_displ = np.cumsum(send_count) - send_count
@@ -65,6 +67,6 @@ def repartition(arr, ndesired, comm, comm_rank, comm_size):
     recv_displ = np.cumsum(recv_count) - recv_count
     arr_return = np.ndarray(sum(recv_count), dtype=arr.dtype)
     my_alltoallv(arr, send_count, send_displ, arr_return, recv_count, recv_displ, comm,
-            comm_rank, comm_size)
+                 comm_rank, comm_size)
 
     return arr_return
