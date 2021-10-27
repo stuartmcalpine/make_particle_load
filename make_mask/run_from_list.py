a5c82c0d03116e0e353922e78950c397df000ce0
Yannick Bahe <bahe@strw.leidenuniv.nl>
2021-10-27 13:12:48 +0200

    Updated run_from_list.py

--- make_mask/run_from_list.py
+++ make_mask/run_from_list.py
@@ -1,83 +1,95 @@
-# Run with:
-# python3 run_from_list.py \
-#   --template param_files/default_sept.yml \
-#   --listfile /cosma7/data/dp004/dc-alta2/xl-zooms/ics/masks/groupnumbers_defaultSept.txt
+"""
+Script to batch process multiple haloes from one parent simulation.
 
+Run as:
+```
+python3 run_from_list.py \
+   --template [name of parameter template file] \
+   --listfile [name of file containing the group numbers to process]
+```
+"""
 import argparse
 import os
 import re
 from shutil import copyfile
 from numpy import loadtxt, ndarray
-from yaml import load
+from yaml import safe_load as load
+from typing import Tuple
 
 from make_mask import MakeMask
 
-this_file_directory = os.path.dirname(__file__)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--template', action='store', type=str)
-parser.add_argument('--listfile', action='store', type=str)
-args = parser.parse_args()
+def make_masks_from_list() -> None:
+    """Main wrapper function to generate the masks."""
+
+    parser = argparse.ArgumentParser(
+        description="Script to batch process multiple haloes from the same "
+                    "parent simulation."
+    )
+    parser.add_argument('-t', '--template_file',
+        help="The name of the template parameter file to generate the mask, "
+             "relative to the directory containing this script.")
+    parser.add_argument('-l', '--list_file',
+        help="The name of the file containing the group numbers to process, "
+             "relative to the directory containing this script.")
+    args = parser.parse_args()
+
+    # Parse the parameter (template), and check the expected placeholders
+    params = load(open(args.template_file))
+    if 'GROUPNUMBER' not in params['fname']:
+        raise ValueError(
+            "'fname' parameter in the template file must contain the "
+            f"placeholder 'GROUPNUMBER', but is '{params['fname']}'!"
+        )
+
+    # Copy the template parameter file to the output directory    
+    out_dir = get_output_dir_from_template()    # out_dir is created if needed
+    this_file_directory = os.path.dirname(__file__)
+    copyfile(
+        os.path.join(this_file_directory, args.template_file),
+        os.path.join(out_dir, os.path.basename(args.template_file))
+    )
+
+    group_numbers, sorter = get_group_numbers_list()
+    if sorter is not None and 'sort_type' in params:
+        if sorter.lower() != params['sort_type'].lower():
+            print(
+                f"Warning: list file appears to refer to sort_type "
+                f"'{sorter}', but parameter file specifies sort_type "
+                f"'{params['sort_type']}'. Overriding parameter file..."
+            )
+
+    for group in group_numbers:
+        params_grp = copy.deepcopy(params)
+        params_grp['fname'].replace('GROUPNUMBER', str(group_number))
+        params_grp['group_number'] = group_number
+        mask = MakeMask(params=params_grp)
 
 
 def get_output_dir_from_template() -> str:
     params = load(open(args.template))
     output_dir = params['output_dir']
     if not os.path.isdir(output_dir):
-        raise OSError(f"The specified output directory does not exist. Trying to save to {output_dir}")
+        os.path.makedirs(output_dir)
     return output_dir
 
 
-def get_run_name_from_template() -> str:
-    params = load(open(args.template))
-    return params['fname']
+def get_group_numbers_list(list_file: str) -> Tuple[np.ndarray, str]:
+    # Check whether the first line contains a header indicating the type
+    # of sorter (M200c, M500c)
+    with open(list_file) as f:
+        header = f.readline()
+        if header[0] == '#':
+            if 'm200crit' in header.lower():
+                sorter = 'm200crit'
+            elif 'm500crit' in header.lower():
+                sorter = 'm500crit'
+            elif 'none' in header.lower():
+                sorter = 'none'
+            else:
+                sorter = None
 
-
-def get_group_numbers_list() -> ndarray:
-    return loadtxt(args.listfile).astype(int)
-
-
-def get_mass_sort_key() -> str:
-    with open(args.listfile, "r") as selection:
-        lines = selection.readlines()
-        for line in lines:
-            if 'mass_sort' in line:
-                sort_key = line.split()[-1]
-                break
-    assert '500' in sort_key or '200' in sort_key, ("Mass sort key returned unexpected value.",
-                                                    f"Expected `M_200crit` or `M_500crit`, got {sort_key}")
-    return sort_key
-
-
-def replace_pattern(pattern: str, replacement: str, filepath: str):
-    with open(filepath, "r") as sources:
-        lines = sources.readlines()
-    with open(filepath, "w") as sources:
-        for line in lines:
-            sources.write(re.sub(pattern, replacement, line))
-
-
-def make_masks_from_list() -> None:
-    out_dir = get_output_dir_from_template()
-    copyfile(
-        os.path.join(this_file_directory, args.template),
-        os.path.join(out_dir, os.path.basename(args.template))
-    )
-
-    for group_number in get_group_numbers_list():
-
-        mask_name = get_run_name_from_template().replace('GROUPNUMBER', str(group_number))
-        mask_paramfile = os.path.join(out_dir, f"{mask_name}.yml")
-        copyfile(os.path.join(out_dir, os.path.basename(args.template)), mask_paramfile)
-        replace_pattern('GROUPNUMBER', str(group_number), mask_paramfile)
-        sort_key = get_mass_sort_key()
-        if '500' in sort_key:
-            replace_pattern('SORTM200', '0', mask_paramfile)
-            replace_pattern('SORTM500', '1', mask_paramfile)
-        elif '200' in sort_key:
-            replace_pattern('SORTM200', '1', mask_paramfile)
-            replace_pattern('SORTM500', '0', mask_paramfile)
-        mask = MakeMask(mask_paramfile)
+    return loadtxt(list_file).astype(int), sorter
 
 
 if __name__ == '__main__':
